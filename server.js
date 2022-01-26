f7086409546887f7694c4faa542041f1d6e64e22
PleaBanshee <llewellynant@gmail.com>
2022-01-26 14:47:08 +0200

    Added security headers

--- server.js
+++ server.js
@@ -12,7 +12,15 @@ app.use(helmet())
 app.use(express.json()) // you no longer need body-parser package...it comes built into expressJS now
 app.use(morgan('combined'));
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+  // (name, value, {options}): creates a cookie to store server data. Headers prevents cookie from being accessed by client and attackers
+  res.cookie('session','1',{httpOnly: true,secure: true});
+  // only allows scripts to be run on the host and others provided, thus preventing CSRF
+  res.set({
+    'Content-Security-Policy': "script-src 'self' 'https://developers.google.com/apis-explorer'"
+  });
+  res.send('Hello World!')
+})
 
 app.post('/secret', (req, res) => {
   const { userInput } = req.body;
